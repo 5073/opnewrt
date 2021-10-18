99f03562385ffd091fe4360f983ef4e6e943aeeb
5073 <71521865+5073@users.noreply.github.com>
2021-10-18 13:44:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.10.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 
 
 
