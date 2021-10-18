8de03567674cce2c22afe02533ce27ec67f81adf
5073 <71521865+5073@users.noreply.github.com>
2021-10-18 14:27:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,7 @@ sed -i 's/192.168.10.1/192.168.50.5/g' package/base-files/files/bin/config_gener
 
 
 
-
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+sed -i "s/OpenWrt /2021/10/16bao compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
