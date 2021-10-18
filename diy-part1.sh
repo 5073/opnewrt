9f20e235306d3b9e9a022c750f5dfadbfaaf5dc6
5073 <71521865+5073@users.noreply.github.com>
2021-10-18 14:26:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-cd /lede/package/lean/
-gti clone https://github.com/Warm-rain/giwifi.git package/lean/giwifi
-gti clone https://github.com/hyrathb/mentohust.git package/lean/mentohust
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
